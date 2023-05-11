50f62caefc209dccd1bff9bf2123d08a07c98fc0
naporin0624 <naporin0624@gmail.com>
2023-05-11 21:44:20 +0900

    :+1: add nav

--- App.tsx
+++ App.tsx
@@ -1,24 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { StatusBar } from 'expo-status-bar'
+import { useCallback } from 'react'
+import { Text, Button } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <Text>{process.env.NODE_ENV}</Text>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+import SafeArea from '@components/safe-area'
+
+import type { NativeStackScreenProps } from '@react-navigation/native-stack'
+import type { FC } from 'react'
+
+type RootStackParamList = {
+  Home: {
+    id: string
+  }
+  Setting: undefined
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    background: '#2d2d2d',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-});
+const Home: FC<NativeStackScreenProps<RootStackParamList, "Home">> = (({ navigation, route }) => {
+  const handleClick = useCallback(() => {
+    navigation.push('Setting')
+  }, [navigation])
+
+  return (
+    <SafeArea>
+      <StatusBar style="dark" />
+      <Text style={{ width: '100%' }}>Home</Text>
+      <Text style={{ width: '100%' }}>{route.params.id}</Text>
+      <Button title="button" onPress={handleClick} />
+    </SafeArea>
+  )
+})
+
+const Setting: FC<NativeStackScreenProps<RootStackParamList, "Setting">> = ({ navigation }) => {
+  const handleClick = useCallback(() => {
+    navigation.push('Home', { id: Date.now().toString() })
+  }, [navigation])
+
+  return (
+    <SafeArea>
+      <StatusBar style="dark" />
+      <Text>Setting</Text>
+      <Button title="button" onPress={handleClick} />
+    </SafeArea>
+  )
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar style="light" />
+        <Stack.Navigator screenOptions={{ headerShown: true }} initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} initialParams={{ id: '' }} />
+          <Stack.Screen name="Setting" component={Setting} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  )
+}
+
+export default App
