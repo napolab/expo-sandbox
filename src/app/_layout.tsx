544a88fbf098bbe7a1096e6d86430f9e2d344c0b
naporin0624 <naporin0624@gmail.com>
2023-05-22 11:03:15 +0900

    :+1: font まわりを修正

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { useFonts, NotoSansJP_400Regular } from '@expo-google-fonts/noto-sans-jp'
+import { useFonts, NotoSansJP_400Regular as NotoSansJPRegular } from '@expo-google-fonts/noto-sans-jp'
 import { SplashScreen, Stack } from 'expo-router'
 import { memo } from 'react'
 import { Provider } from 'react-flowder'
@@ -8,12 +8,11 @@ import { theme } from '@theme/config'
 import type { FC } from 'react'
 
 const Layout: FC = () => {
-  const [fontLoaded, error] = useFonts({
-    // eslint-disable-next-line camelcase
-    [theme.font.NotoSansJP[400]]: NotoSansJP_400Regular,
+  const [fontLoaded] = useFonts({
+    [theme.font.NotoSansJP[400]]: NotoSansJPRegular,
   })
 
-  if (!fontLoaded) return <SplashScreen />
+  if (!fontLoaded ) return <SplashScreen />
 
   return (
     <Provider>
